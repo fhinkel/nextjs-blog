a85737bb9304858400fb04136d63c3090bd993c9
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2022-08-15 11:39:33 -0400

    Fix title

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Franziska Hinkelmann, Ph.D.</title>
         </Head>
         <Container>
           <Intro />
