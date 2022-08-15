fb9b70829828dd20cc5d03f27c98e54dbb9911c4
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2022-08-15 08:10:53 -0400

    Update alert to point to coaching

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,6 +1,6 @@
 import Container from './container'
 import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { COACHING } from '../lib/constants'
 
 type Props = {
   preview?: boolean
@@ -29,14 +29,14 @@ const Alert = ({ preview }: Props) => {
             </>
           ) : (
             <>
-              The source code for this blog is{' '}
+              Free {' '}
               <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+                href={`${COACHING}`}
                 className="underline hover:text-blue-600 duration-200 transition-colors"
               >
-                available on GitHub
+                career coaching
               </a>
-              .
+              {' '}for women in tech.
             </>
           )}
         </div>
