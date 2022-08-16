b6c987ee00cbb57728347fd356afc5f140125281
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2022-08-16 11:20:43 -0400

    Use rehypeFigure for captions

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,11 @@
 import { remark } from 'remark'
-import html from 'remark-html'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+import rehypeFigure from 'rehype-figure'
+
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark().use(remarkRehype).use(rehypeStringify)
+    .use(rehypeFigure, { className: "my-figure" }).process(markdown)
   return result.toString()
 }
