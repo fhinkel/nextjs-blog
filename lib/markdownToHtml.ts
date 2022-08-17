9bb095d60bfd710bb7e2933b22e8bf1b58b66e57
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2022-08-16 20:52:31 -0400

    Use prism.js for syntax highlighting

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,10 +2,15 @@ import { remark } from 'remark'
 import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
 import rehypeFigure from 'rehype-figure'
+import remarkPrism from "remark-prism";
 
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(remarkRehype).use(rehypeStringify)
-    .use(rehypeFigure, { className: "my-figure" }).process(markdown)
+  const result = await remark()
+    .use(remarkPrism)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .use(rehypeFigure, { className: "my-figure" })
+    .process(markdown)
   return result.toString()
 }
