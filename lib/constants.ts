5d955cd741324fa34cc94edece5a2779fb7d1d4b
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2022-08-16 21:27:47 -0400

    Switch OG image to jpeg

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const COACHING = 'https://www.fhinkel.rocks/coaching/'
 export const GITHUB = 'https://github.com/fhinkel/nextjs-blog'
-export const HOME_OG_IMAGE_URL = 'https://twitter.com/fhinkel/header_photo'
+export const HOME_OG_IMAGE_URL = 'https://nextjs-blog-fhinkel.vercel.app/assets/blog/authors/franziska.jpeg'
