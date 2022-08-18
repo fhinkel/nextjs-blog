4073f2fe37ff5cfed1d84174967b2f012828cd54
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2022-08-18 10:41:42 -0400

    Use raw iframes for YouTube

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -3,16 +3,15 @@ import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
 import rehypeFigure from 'rehype-figure'
 import remarkPrism from "remark-prism";
-import remarkOembed from "remark-oembed";
+
 
 
 export default async function markdownToHtml(markdown: string) {
   let result = await remark()
     .use(remarkPrism)
-    .use(remarkOembed)
-    .use(remarkRehype,  {allowDangerousHtml: true})
+    .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeFigure, { className: "my-figure" })
-    .use(rehypeStringify,  {allowDangerousHtml: true})
+    .use(rehypeStringify, { allowDangerousHtml: true })
     .process(markdown)
 
   return result.toString()
